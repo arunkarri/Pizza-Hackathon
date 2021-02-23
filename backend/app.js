1b6769f5cb6a1cf0598f2f48023d1befe810bde2
Arun Kashyap Karri <arun.kashyap@dltlabs.io>
2021-02-23 14:03:12 +0530

    updated cors settings

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,14 @@ const pizzaRouter = require('./routes/pizza');
 var app = express();
 
 // view engine setup
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(cors());
@@ -20,18 +28,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
 app.use('/pizza', pizzaRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
